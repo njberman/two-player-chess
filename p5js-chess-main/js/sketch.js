33b4eba3a84aedc471466d5410c471f58f9f645a
njberman <njberman16@hotmail.com>
2022-12-20 14:59:07 +0000

    copy FEN to clipboaord

--- p5js-chess-main/js/sketch.js
+++ p5js-chess-main/js/sketch.js
@@ -1,5 +1,6 @@
 import Board from "./Board.js";
 import { SIZE } from "./constants.js";
+import { convertToFen } from "./convertToFen.js";
 let board;
 
 window.setup = () => {
@@ -16,4 +17,6 @@ window.onclick = function (evt) {
   const x = evt.clientX;
   const y = evt.clientY;
   board.userClick(x, y);
+  navigator.clipboard.writeText(convertToFen(board));
+  // console.log(convertToFen(board));
 };
